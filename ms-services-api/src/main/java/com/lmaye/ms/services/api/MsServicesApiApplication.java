6aecaa73e228bbf131da3ef04980bd38c3c2c446
lmay <lmay@lmaye.com>
2020-09-23 14:06:21 +0800

    -- add api model.

---
+++ ms-services-api/src/main/java/com/lmaye/ms/services/api/MsServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.lmaye.ms.services.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServicesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsServicesApiApplication.class, args);
+    }
+
+}
---
+++ ms-services-api/src/test/java/com/lmaye/ms/services/api/MsServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.ms.services.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
