667d7af66f224bd0d8b9c091a2839da700e490f8
lmay <lmay@lmaye.com>
2020-12-17 14:32:37 +0800

    -- feat: init project.

---
+++ src/main/java/com/lmaye/netty/examples/NettyExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.lmaye.netty.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmaye/netty/examples/NettyExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmaye.netty.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
